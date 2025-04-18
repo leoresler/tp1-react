81595646a1c4ea38f89740be62cda44058163c07
tiinch00 <mdep171@gmail.com>
2025-04-18 18:58:50 -0300

    Buscador

--- components/Buscador/Buscador.jsx
+++ components/Buscador/Buscador.jsx
@@ -1,23 +1,51 @@
-import  styles  from './Buscador.module.css';       // se usa styles cuando se usa className
+import { useState, useEffect } from "react";
+import styles from "./Buscador.module.css";
+import ItemListContainer from "../../../../mi-app-react/src/componentes/ItemListContainer/ItemListContainer.jsx";
+import data from "../../../../mi-app-react/src/data/datos.json";//datos de mi json(arreglo de peliculas)
 
+const Buscador = () => {
+  const [busqueda, setBusqueda] = useState("");
+  const [peliculas, setPeliculas] = useState([]);
 
+  const buscarPelicula = (e) => {
+    setBusqueda(e.target.value);
+  };
 
+  const pedirPeliculas = () => {
+    return new Promise((resolve) => {
+      resolve(data);
+    });
+  };
 
-export const Buscador = ({busqueda, setBusqueda }) => {
+  useEffect(() => {
+    pedirPeliculas().then((resp) => {
+      setPeliculas(resp);
+    });
+  }, []);
 
-
+  const peliculasAMostrar = busqueda.trim()
+    ? peliculas.filter((dato) =>
+        dato.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
+        dato.director.toLowerCase().includes(busqueda.toLowerCase())
+      )
+    : peliculas;
 
   return (
-    <div>
-    <label htmlFor="busqueda">Buscador:</label><br />
-    <input
-     id="busqueda"
-     type="text"
-     placeholder="ingresa pelicula"
-     value={busqueda}
-     onChange={(e) => setBusqueda(e.target.value)}
-     className={styles.inputBuscador}                   //se usa styles.nombreclase por el "module" 
-    />
+    <div className={styles.divBuscador}>
+      <label className={styles.labelBuscador} htmlFor="busqueda">
+        Buscador de Películas
+      </label>
+      <input
+        className={styles.inputBuscador}
+        id="busqueda"
+        type="text"
+        placeholder="Ingresá el título o director..."
+        value={busqueda}
+        onChange={buscarPelicula}
+      />
+      <ItemListContainer peliculas={peliculasAMostrar} />
     </div>
-  )
-}
+  );
+};
+
+export default Buscador;
