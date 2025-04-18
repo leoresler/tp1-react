f542557256bc8cadb7979a7d4a132a8dd4c356db
leoresler <leandroresler4@gmail.com>
2025-04-18 19:50:52 -0300

    formulario agregar peliculas

---
+++ components/FormAgregarPeliculas/FormAgregarPeliculas.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import "./FormAgregarPeliculas.module.css";
+
+const FormAgregarPeliculas = ({onAgregar}) => {
+  const [nuevaPelicula, setNuevaPelicula] = useState({
+    titulo: "",
+    director: "",
+    año: "",
+    genero: "",
+    tipo: "",
+    rating: "",
+  });
+
+  const handleChange = (e) => {
+    setNuevaPelicula({
+      ...nuevaPelicula,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAgregar({ ...nuevaPelicula, id: crypto.randomUUID() });
+    setNuevaPelicula({
+      titulo: "",
+      director: "",
+      año: "",
+      genero: "",
+      tipo: "",
+      rating: "",
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="titulo"
+        placeholder="Título"
+        value={nuevaPelicula.titulo}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="director"
+        placeholder="Director"
+        value={nuevaPelicula.director}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="año"
+        placeholder="Año"
+        value={nuevaPelicula.año}
+        onChange={handleChange}
+      />
+      <select
+        name="genero"
+        value={nuevaPelicula.genero}
+        onChange={handleChange}
+      >
+        <option value="">Selecciona un género</option>
+        <option value="Acción">Acción</option>
+        <option value="Comedia">Comedia</option>
+        <option value="Drama">Drama</option>
+        <option value="Ciencia Ficción">Ciencia Ficción</option>
+        <option value="Terror">Terror</option>
+        <option value="Fantasía">Fantasía</option>
+      </select>
+
+      <select
+        name="tipo"
+        value={nuevaPelicula.tipo}
+        onChange={handleChange}
+      >
+        <option value="">Tipo (película/Serie)</option>
+        <option value="Pelicula">Película</option>
+        <option value="Serie">Serie</option>
+      </select>
+      <input
+        type="text"
+        name="rating"
+        placeholder="Rating"
+        value={nuevaPelicula.rating}
+        onChange={handleChange}
+      />
+      <button type="submit">Agregar Película</button>
+    </form>
+  );
+};
+
+export default FormAgregarPeliculas;
