1d8e3c142c94ad54c9a6faf0a7ff89b7d38b769c
Francisco Pandolfi Jimenez <francisco.pandolfi@est.fi.uncoma.edu.ar>
2025-04-19 17:23:26 -0300

    Agregue Componentes Botones (editar y eliminar) en Button.jsx. Tambien pude agregar la funcionalidad desde el buscador borrar y que cuando una lista se actulice, tambien se acutalice en la lista por ver

--- components/Buscador/Buscador.jsx
+++ components/Buscador/Buscador.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
-import styles from "./Buscador.module.css";
+import { useEffect, useState } from "react";
+
 import ListaPeliculas from "../ListaPeliculas/ListaPeliculas.jsx";
-import ItemListContainer from "../../../../mi-app-react/src/componentes/ItemListContainer/ItemListContainer.jsx";
+import styles from "./Buscador.module.css";
+
+//import ItemListContainer from "../../../../mi-app-react/src/componentes/ItemListContainer/ItemListContainer.jsx";
 //import data from "../../../../mi-app-react/src/data/datos.json";//datos de mi json(arreglo de peliculas)
 //import peliculasData from "../../src/assets/peliculas";
 
-const Buscador = ({peliculas, agregarVista,
-  editarItem}) => {
+const Buscador = ({ peliculas, agregarVista,
+  editarItem, eliminarItem }) => {
   const [busqueda, setBusqueda] = useState("");
   //const [peliculas, setPeliculas] = useState(peliculasData);
 
@@ -17,45 +19,43 @@ const Buscador = ({peliculas, agregarVista,
 
   const peliculasFiltradas = busqueda.trim()
     ? peliculas.filter((dato) =>
-        dato.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
-        dato.director.toLowerCase().includes(busqueda.toLowerCase())
-      )
+      dato.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
+      dato.director.toLowerCase().includes(busqueda.toLowerCase())
+    )
     : peliculas;
 
-    console.log(peliculasFiltradas);
-
-    
+  //console.log(peliculasFiltradas);
 
   return (
     <>
-    <div className={styles.divBuscador}>
-      <label className={styles.labelBuscador} htmlFor="busqueda">
-        Buscador de Películas
-      </label>
-      <input
-        className={styles.inputBuscador}
-        id="busqueda"
-        type="text"
-        placeholder="Ingresá el título o director..."
-        value={busqueda}
-        onChange={buscarPelicula}
-      />
+      <div className={styles.divBuscador}>
+        <label className={styles.labelBuscador} htmlFor="busqueda">
+          Buscador de Películas
+        </label>
+        <input
+          className={styles.inputBuscador}
+          id="busqueda"
+          type="text"
+          placeholder="Ingresá el título o director..."
+          value={busqueda}
+          onChange={buscarPelicula}
+        />
       </div>
       <div className={styles.divBusqueda}>
 
-       {busqueda && (
-        <ListaPeliculas
-          titulo="Resultados de búsqueda"
-          peliculas={peliculasFiltradas}
-          agregarVista ={agregarVista}
-          editarItem = {editarItem}
-         
-        />
-        
-      )}
+        {busqueda && (
+          <ListaPeliculas
+            titulo="Resultados de búsqueda"
+            peliculas={peliculasFiltradas}
+            agregarVista={agregarVista}
+            editarItem={editarItem}
+            eliminarItem={eliminarItem}
+          />
+
+        )}
       </div>
-      
-      
+
+
     </>
   );
 };
--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -13,3 +13,23 @@ export const Button = ({ vista, porVer, arreglo, nombre }) => {
     <button onClick={handleClick}>{nombre}</button>
   );
 };
+
+export const BotonEliminar = ({ nombre, elemento, funcion }) => {
+  const handleEliminar = () => {
+    funcion(elemento.id);
+  }
+    
+  return (
+    <button onClick={handleEliminar}>{nombre}</button>
+  );
+};
+
+export const BotonEditar = ({ nombre, funcion }) => {
+  const handleEditar = () => {
+    funcion();
+  }
+    
+  return (
+    <button onClick={handleEditar}>{nombre}</button>
+  );
+};
